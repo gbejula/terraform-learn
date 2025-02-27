bb39d10df5a5294f9063bf451f210c5657e2920d
Olugbenga Ajayi <gbejula@gmail.com>
2025-02-27 11:28:59 +0100

    corrected route error

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_internet_gateway" "myapp-igw" {
 resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
